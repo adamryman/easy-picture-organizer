ac38f6e7ee0990288c95ddb3251b25afbf33e4f5
Adam Ryman <adamryman@gmail.com>
2012-11-01 16:35:52 -0700

    Changed how it fixes metadata entirely, it was not working before on most cases

--- sortImages.py
+++ sortImages.py
@@ -7,7 +7,7 @@ import time
 currentPath = "" + os.getcwd()
 
 def getMonth(month):
-	months = ["01 - Jan", "01 - Feb", "03 - Mar", "04 - Apr",
+	months = ["01 - Jan", "02 - Feb", "03 - Mar", "04 - Apr",
 	"05 - May", "06 - Jun", "07 - July", "08 - Aug", "09 - Sept",
 	 "10 - Oct", "11 - Nov", "12 - Dec"]
 	month = int(month)
@@ -61,32 +61,57 @@ def sortImages():
 	print("Finish Sorting")
 
 def fixMetaData():
+
+	#Damn, need to fix cheaty way of finding date and spliting the file
+	#I should loop for File name and Date/Time instead of magic numbers
+
+
 	print("")
 	print("Fixing MetaData")
 	#Get the System output
 	allPictures = os.popen(currentPath + "/./Jhead *").read()
 	#Break it into sections based on collens, it is easy to get the date that way
-	allPicturesBrokenApart = allPictures.split(":")
+	allPicturesBrokenApart = allPictures.split("\n")
+	print(allPicturesBrokenApart)
+	print("")
+	print("")
 	#Remove the last value to make it even
-	allPicturesBrokenApart.remove(allPicturesBrokenApart[len(allPicturesBrokenApart) - 1])
 	fileInfoList= []
-	i = 0
-	nextNumber = 0
-	#Break the huge list into lists for each file, each one has 15 fields and that is why we break at 15
-	while( i < len(allPicturesBrokenApart)/15):
-		fileInfoList.append(allPicturesBrokenApart[nextNumber:nextNumber + 15])
-		nextNumber = nextNumber + 15
-		i = i + 1
+
+	lastValue = 0
+	for i in range(len(allPicturesBrokenApart)):
+		if allPicturesBrokenApart[i] == "":
+			fileInfoList.append(allPicturesBrokenApart[lastValue:i])
+			lastValue = i
+
+	fileInfoList.remove([""])
 	#For each file we want to get its name, fix it's metadata (the YYYY:MM:DD format seems to be broken sometimes and is YYYY:MMDD:DD)
 	#I take the :MMDD: (which is at index 11) and only take the first two values which is MM and replace the meta data with a new YYYY:DD
+	
+	print("Starting new stuff")
 	for files in fileInfoList:
+		name = ""
+		date = ""
+		for items in files:
+			isFileName = items.find("File name    :")
+			isDateTime = items.find("Date/Time    :")
+			if not isFileName == -1:
+				name = items.split(":")[1]
+				name = name[1:name.__len__()]
+			if not isDateTime == -1:
+				date = items.split(":")[1] + ":" + items.split(":")[2]
+				date = date[1:8]
+		print(name)
+		print(date)
+		fixMetaDataCommand = currentPath + "/./Jhead -ds" + date + " " + name
+		os.system(fixMetaDataCommand)
+		os.system(fixMetaDataCommand)
+		os.system(fixMetaDataCommand)
+		print("")
+
 		#The name is at index 1, just getting rid of the stuff around it
-		name = files[1].split("\n")
-		name = name[0][1:name[0].__len__()]
-		fixMetaDataCommand = currentPath + "/./Jhead -ds" + files[10][1:5] + ":" + files[11][0:2] + " " + name
+		
 		#For some reason it only works if I do it twice
-		os.system(fixMetaDataCommand)
-		os.system(fixMetaDataCommand)
 	print("Finished fixing MetaData")
 	print("")
 
